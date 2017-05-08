a46f2fc9fda0332de332fd76d888c2c33f8db8b3
rampage1xx <godhasu@gmail.com>
2017-05-08 22:34:29 +0200

    More tests

---
+++ Client/src/Component/Card/tests.tsx
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import {CardBottom} from './CardBottom';
+import {CardBodyText} from './CardBodyText';
+import {mount, shallow} from 'enzyme';
+import {CardDelete} from './CardDelete';
+import {CardSubtitle, CardText, CardTitle} from 'reactstrap';
+const title = 'title';
+const description = 'description';
+
+declare const describe;
+declare const it;
+declare const expect;
+
+describe('Card Body Text', () => {
+    const ShallowCardBody = shallow(<CardBodyText description={ description } title={ title }/>);
+    const MountCardBody = mount(<CardBodyText description={ description } title={ title }/>);
+
+    it('renders correctly the tile', () => {
+        expect(ShallowCardBody.contains(<CardTitle>title</CardTitle>)).toBe(true);
+        expect(ShallowCardBody.contains(<CardText>description</CardText>)).toBe(true);
+    });
+
+    it('should mount in a full DOM', () => {
+
+        expect(MountCardBody.find(CardBodyText)).toHaveLength(1);
+        expect(MountCardBody.props().description).toEqual(description);
+        expect(MountCardBody.props().title).toEqual(title);
+    });
+
+    it('should not render', () => {
+        const MountDeleteCard = shallow(<CardDelete deletePin={ false }/>);
+        expect(MountDeleteCard.isEmptyRender()).toEqual(true);
+    });
+
+});
+
+describe('card bottom tests', () => {
+    const dummyFunction = (): any => {
+        return 'test';
+    };
+    const wrapperShallowCardBottom = shallow(
+        <CardBottom avatar={ 'avatar' } totalLikes={ 2 } like={ true } userName={ 'user' }
+                    findUserHandler={ dummyFunction } likeHandler={ dummyFunction }/>
+    );
+    it('should render total likes and like status', () => {
+        expect(wrapperShallowCardBottom.find('.faLightGreen').exists()).toBe(true);
+        expect(wrapperShallowCardBottom.find('.totalLikes__card__p').contains(2)).toBe(true);
+
+    });
+});
---
+++ Client/src/Utils/tests.tsx
@@ -0,0 +1,45 @@
+import * as React from 'react';
+import {GenerateMasonry} from './GenerateMasonry';
+import {imagesArrayTest, likesArrayTest, user_id} from '../../tests/mocks/variables';
+import {shallow} from 'enzyme';
+import {Row} from '../Component/Row/Row';
+import {addLikeStatusToArray} from './GraphQL/Queries';
+const fakeFunction = (index: number) => void {};
+const {imagesListGraphQL} = imagesArrayTest.data;
+
+declare const describe;
+declare const it;
+declare const expect;
+
+describe('testing masonry creation', () => {
+
+    it(' should generate a  masonry of 24 pieces', () => {
+        const result = GenerateMasonry({
+            id: user_id,
+            deleteCardFunction: fakeFunction,
+            deletePin: false,
+            images: imagesListGraphQL
+        });
+        expect(result.length).toBe(24);
+        expect(result[11].props.LoggedInUserID)
+            .toContain('b7a302ae-6acf-4bf1-8bf6-bba0143a6d5c');
+    });
+
+    it('should inject an array of images into the row component', () => {
+        const shallowRenderRow = shallow(<Row pins={ imagesListGraphQL }/>);
+        expect((shallowRenderRow.props().children.length)).toBe(24);
+    });
+});
+
+describe('testing apollo functions', () => {
+    it('should add like status to images', () => {
+        const result = addLikeStatusToArray(imagesListGraphQL, likesArrayTest);
+        expect(result[1].like).toBe(true);
+    });
+
+    it('should return an empty array', () => {
+        const result = addLikeStatusToArray([], []);
+        expect(result).toEqual([]);
+    });
+
+});
