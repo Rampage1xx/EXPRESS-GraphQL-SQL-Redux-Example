3fdef7f3ed18f63a1e762d73aa9c3b37cf7093a5
rampage1xx <godhasu@gmail.com>
2017-05-19 20:54:39 +0200

    Setting up the config file for the server

---
+++ Backend/config/keys.ts
@@ -0,0 +1,11 @@
+
+// bind those const to  ENV variables
+
+export const googleClientID = 'googleClientID';
+export const googleClientSecret = 'googleClientSecret';
+
+export const facebookClientID = 'facebookClientID';
+export const facebookClientSecret = 'facebookClientSecret';
+
+export const twitterConsumer = 'twitterConsumer';
+export const twitterSecret = 'twitterSecret'
